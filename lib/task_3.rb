3a10df7b3d6d4bcbc8bb739340d5360837b0b21b
Garik <garfild_2003@mail.ru>
2024-03-12 12:26:23 +0300

    Finished task 3

--- lib/task_3.rb
+++ lib/task_3.rb
@@ -1,2 +1,34 @@
-# frozen_string_literal: false
+require 'prime'
 
+
+def prime_with_require(n)
+  raise ArgumentError, "N не может быть отрицательным или равняться нулю, для таких значений результатом будет пустой массив" if n <= 0
+
+  result = "Первые #{n} простых чисел: #{(Prime.first(n))}"
+end
+
+
+#Находит N первых простых чисел
+#
+# @param [Integer] n Количество простых чисел, которое нужно найти
+# @return [String] Строка, содержащая результат работы программы
+# @raise [ArgumentError] Возникает, если N <= 0
+# Пример:
+#
+#   input: 5
+#   output: "Первые 5 простых чисел: [2, 3, 5, 7, 11]"
+#
+def prime(n)
+  raise ArgumentError, "N не может быть отрицательным или равняться нулю, для таких значений результатом будет пустой массив" if n <= 0
+
+  new_arr = []
+  counter = 2
+
+  while new_arr.size < n
+    if (2..counter/2).none?{|j| counter % j == 0}
+      new_arr << counter
+    end
+    counter += 1
+  end
+  result = "Первые #{n} простых чисел: #{new_arr.inspect}"
+end
